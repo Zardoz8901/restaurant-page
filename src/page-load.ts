8a8b34449137335d09ebecab6540cd55c897b45c
Zardoz8901 <interzoneclearinghouse@gmail.com>
2023-09-13 19:28:20 +0200

    Refactor and reduce cruft

--- src/page-load.ts
+++ src/page-load.ts
@@ -1,27 +1,5 @@
 export default function pageLoad() {
     const body = document.querySelector('body');
-    const createDiv = () => {
-        const div = document.createElement('div');
-        return div;
-    };
-    const contentWrapper = createDiv();
-    const headerNavBar = createDiv();
-    const reservationButton = createDiv();
-    const headerDinnerMenu = createDiv();
-    const aLaCarte = createDiv();
-    const allergyDisclaimer = createDiv();
-    const preFooterImage = createDiv();
-    const footerNavBar = createDiv();
-    const divArray = [
-        contentWrapper,
-        headerNavBar,
-        reservationButton,
-        headerDinnerMenu,
-        aLaCarte,
-        allergyDisclaimer,
-        preFooterImage,
-        footerNavBar,
-    ];
     const idArray = [
         'content-wrapper',
         'header-nav-bar',
@@ -32,16 +10,14 @@ export default function pageLoad() {
         'pre-footer-image',
         'footer-nav-bar',
     ];
-    divArray.forEach((e, i) => {
-        body.appendChild(e);
-        e.setAttribute('id', idArray[i]);
+    idArray.forEach((i) => {
+        const div = document.createElement('div');
+        body.appendChild(div);
+        div.setAttribute('id', i);
     });
+    const contentWrapper = document.getElementById('content-wrapper');
+    const divArray = Array.from(document.querySelectorAll('div'));
     divArray.slice(1).forEach((e) => {
         contentWrapper.appendChild(e);
     });
-
-    // contentWrapper.setAttribute('id', 'content-wrapper');
-    // body.appendChild(contentWrapper);
-    // headerNavBar.setAttribute('id', 'header-nav-bar');
-    // contentWrapper.appendChild(headerNavBar);
 }
