040d9611e3f02def8b949bb43c95f5c51b637ed3
Zardoz8901 <interzoneclearinghouse@gmail.com>
2023-09-18 12:30:36 +0200

    Instantiate menu items via IIFE

--- src/page-load.ts
+++ src/page-load.ts
@@ -8,6 +8,10 @@ export default function pageLoad() {
         const span = document.createElement('span');
         return span;
     };
+    const createP = () => {
+        const p = document.createElement('p');
+        return p;
+    };
     // instantiate header nav
     const idContentDivArray = [
         'content-wrapper',
@@ -29,6 +33,7 @@ export default function pageLoad() {
         body.appendChild(span);
         span.setAttribute('id', e);
     });
+    // instantiate divs and spans
     const contentWrapper = document.getElementById('content-wrapper');
     const nodeDivArray = Array.from(document.querySelectorAll('div'));
     nodeDivArray.slice(1).forEach((e) => {
@@ -41,21 +46,67 @@ export default function pageLoad() {
         nodeDivArray[1].appendChild(e);
     });
     // instantiate menu section
-    for (let i = 0; i < 8; i += 1) {
-        const menuItem = createDiv();
-        const menuItemTitle = createDiv();
-        const menuItemDescritpion = createDiv();
-        const menuItemPrice = createSpan();
-        const currencySign = createSpan();
-        menuItem.setAttribute('class', 'menu-item');
-        menuItemTitle.setAttribute('class', 'menu-item-title');
-        menuItemDescritpion.setAttribute('class', 'menu-item-description');
-        menuItemPrice.setAttribute('class', 'menu-item-price');
-        currencySign.setAttribute('class', 'currency-sign');
-        nodeDivArray[3].appendChild(menuItem);
-        menuItemPrice.appendChild(currencySign);
-        menuItem.appendChild(menuItemPrice);
-        menuItem.appendChild(menuItemTitle);
-        menuItem.appendChild(menuItemDescritpion);
-    }
+    (() => {
+        for (let i = 0; i < 8; i += 1) {
+            const menuItem = createDiv();
+            const menuItemTitle = createDiv();
+            const menuItemDescritpion = createDiv();
+            const menuItemPrice = createSpan();
+            const currencySign = createSpan();
+            menuItem.setAttribute('class', 'menu-item');
+            menuItemTitle.setAttribute('class', 'menu-item-title');
+            menuItemDescritpion.setAttribute('class', 'menu-item-description');
+            menuItemPrice.setAttribute('class', 'menu-item-price');
+            currencySign.setAttribute('class', 'currency-sign');
+            nodeDivArray[3].appendChild(menuItem);
+            menuItemPrice.appendChild(currencySign);
+            menuItem.appendChild(menuItemPrice);
+            menuItem.appendChild(menuItemTitle);
+            menuItem.appendChild(menuItemDescritpion);
+        }
+        const menuItems = [
+            'Chili Cup',
+            'Hamburber',
+            'Cheese Burger',
+            'Bacon, Lettuce, Tomato',
+            "Chef's Salad",
+            'Tossed Green Salad',
+            'Steak Tartare',
+            'Roast Cornish Hen',
+        ];
+        const menuItemsArray = Array.from(document.getElementsByClassName('menu-item-title'));
+        console.log(menuItemsArray);
+        menuItemsArray.forEach((e, i) => {
+            const div = createDiv();
+            div.setAttribute('class', 'menu-item-text');
+            div.textContent = menuItems[i];
+            e.appendChild(div);
+        });
+        const menuDescriptions = [
+            'Spicy Bulgogi Beef, Gochujang-Infused Beans',
+            'With Sesame Mayo, Fresh Kimchi',
+            'With American Cheese, Gochujang Mayo',
+            'Guanciale, Pickled Daikon',
+            'Crispy Quinoa, Purple Shiso Leaves, Gochujang Dressing',
+            'Watermelon Radish, Yuzu-Infused Avocado, Furikake',
+            'With Quail Egg, Shiso Cress, Black Sesame Crackers',
+            'With Kimchi Pancakes, Pickled Perilla Leaves',
+        ];
+        const menuDescriptionsArray = Array.from(
+            document.getElementsByClassName('menu-item-description')
+        );
+
+        menuDescriptionsArray.forEach((e, i) => {
+            const div = createDiv();
+            div.setAttribute('class', 'menu-description-text');
+            div.textContent = menuDescriptions[i];
+            e.appendChild(div);
+        });
+    })();
+    // instantiate allergy disclaimer
+    const allergyP = createP();
+    allergyP.setAttribute('id', 'allergy-paragraph');
+    nodeDivArray[4].appendChild(allergyP);
+    allergyP.textContent =
+        'Consuming raw or undercooked meats, poultry, seafood, shellfish, or eggs may increase your risk of foodborne illness, especially if you have certain medical conditions.';
 }
